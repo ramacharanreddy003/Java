14c901936bfeb8656b69188fff8106252472f47f
Chinnakambaiah Gari Rama Charan Reddy <ckramcharanreddy03@gmail.com>
2025-12-27 21:53:16 +0530

    Delete hp1.java

--- hp1.java
+++
@@ -1,8 +0,0 @@
-public class hp1 extends SampleAC{
-    public void cut(){
-        System.out.println("HP cut code");
-    }
-    public void keyboard(){
-        System.out.println("key board");
-    }
-}

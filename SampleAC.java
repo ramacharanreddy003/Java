1b37fbd564135c5a1c102c5d4160f64bbe1d279f
Chinnakambaiah Gari Rama Charan Reddy <ckramcharanreddy03@gmail.com>
2025-12-27 21:48:12 +0530

    Add files via upload

---
+++ SampleAC.java
@@ -0,0 +1,12 @@
+public abstract class SampleAC implements laptop1{
+    public void copy(){
+        System.out.println("lenova copy code");
+    }
+    public void paste(){
+        System.out.println("lenova paste code");
+    }
+
+    public  abstract void cut();
+
+    public abstract void keyboard();
+}

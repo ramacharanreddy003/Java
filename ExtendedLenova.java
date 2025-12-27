5c4b60a6aa7cc0e3f5ba71d42ae61ffae595b19a
Chinnakambaiah Gari Rama Charan Reddy <ckramcharanreddy03@gmail.com>
2025-12-27 21:50:39 +0530

    Delete ExtendedLenova.java

--- ExtendedLenova.java
+++
@@ -1,9 +0,0 @@
-public class ExtendedLenova extends lenova1{
-    public void copy(){
-        System.out.println("Extended lenova copy");
-    }
-    
-   /* public void copy(String s){
-        System.out.println("Extended lenovo");
-    }*/
-}

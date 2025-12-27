f5ca10b138246f2286550c8cdb7868571b277cf2
Chinnakambaiah Gari Rama Charan Reddy <ckramcharanreddy03@gmail.com>
2025-12-27 22:12:56 +0530

    Add files via upload

---
+++ Polymorphism/Calculations.java
@@ -0,0 +1,17 @@
+public class Calculations{
+    public void add(int a,int b){
+
+    }
+    public void add(int b,float a,int c){ //method overloading same method but different parameters
+        
+    }
+    public static void main(String[] args){
+        Calculations c = new Calculations();
+        c.add(2,3);
+        c.add(2,3,4);
+    
+        
+    }
+
+    
+}

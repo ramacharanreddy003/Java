9c20ec1023d14eef1f3616b03351d5dbff129ab1
Chinnakambaiah Gari Rama Charan Reddy <ckramcharanreddy03@gmail.com>
2025-12-27 22:21:21 +0530

    Add files via upload

---
+++ ConditionalStatements/ArithmeticOperations.java
@@ -0,0 +1,14 @@
+package ConditionalStatements;
+
+public class ArithmeticOperations {
+	public static void main(String[] args) {
+		System.out.println("charan");
+		int i=0;
+		while(i<5) {
+			System.out.println(i);
+			i++;
+		}
+	}
+	
+
+}
